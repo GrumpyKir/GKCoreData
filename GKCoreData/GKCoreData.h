d957a4e4eea2eb488195923beb0db1db6d2e1695
Kirill Opekishev <grumpykir@gmail.com>
2019-04-12 13:54:37 +0300

    Initial Commit

---
+++ GKCoreData/GKCoreData.h
@@ -0,0 +1,19 @@
+//
+//  GKCoreData.h
+//  GKCoreData
+//
+//  Created by  Кирилл on 4/12/19.
+//  Copyright © 2019 AppCraft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GKCoreData.
+FOUNDATION_EXPORT double GKCoreDataVersionNumber;
+
+//! Project version string for GKCoreData.
+FOUNDATION_EXPORT const unsigned char GKCoreDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GKCoreData/PublicHeader.h>
+
+
